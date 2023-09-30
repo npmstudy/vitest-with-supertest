82b335596a6396860b261312f4a376a644a69440
npmstudy <npmstudy@qq.com>
2023-09-30 11:49:41 +0800

    refact, please ignore

--- index.test.js
+++ index.test.js
@@ -2,14 +2,9 @@ import { expect, test } from "vitest";
 import supertest from "supertest";
 import app from "./index.js";
 
-test("koa app", () => {
-  // expect(sum(1, 2)).toBe(3);
-  supertest(app.callback())
-    .get("/")
-    .expect("Content-Type", /html/)
-    // .expect("Content-Length", "15")
-    .expect(200)
-    .end(function (err, res) {
-      if (err) throw err;
-    });
+test("koa app", async () => {
+  const res = await supertest(app.callback()).get("/");
+
+  expect(res.status).toEqual(200);
+  expect(res.text).toEqual("Hello Koa");
 });
